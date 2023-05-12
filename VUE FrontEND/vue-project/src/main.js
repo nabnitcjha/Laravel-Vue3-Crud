d36ce6fff27d4b08f3c9daab715c182d3ec52029
Nabnit Jha <nabnitcjha@gmil.com>
2023-05-12 17:12:38 +0545

    init push

---
+++ VUE FrontEND/vue-project/src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+
+app.use(createPinia())
+app.use(VueAxios, axios)
+app.provide('axios', app.config.globalProperties.axios)
+app.use(router)
+
+app.mount('#app')
---
+++ VUE FrontEND/vue-project/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { inject } from 'vue';
+
+const axios = inject('axios')
+// reactive state
+const count = ref(0);
+
+// functions that mutate state and trigger updates
+function axiosCall() {
+  debugger;
+  axios
+    .get("http://127.0.0.1:8000")
+    .then((response) => {
+      debugger;
+      console.log(response.data);
+    });
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`);
+});
+</script>
+
+<template>
+  <button @click="axiosCall">Axios Call</button>
+</template>
